2168d6e38b86ca8ce209bec483cfad71f30c0fd1
SondiDk <olle_s_larsen@hotmail.com>
2019-03-15 11:42:55 +0100

    init proj

---
+++ PsyDataLib/PsyDataLib.h
@@ -0,0 +1,19 @@
+//
+//  PsyDataLib.h
+//  PsyDataLib
+//
+//  Created by Oliver Larsen on 15/03/2019.
+//  Copyright © 2019 amsiq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PsyDataLib.
+FOUNDATION_EXPORT double PsyDataLibVersionNumber;
+
+//! Project version string for PsyDataLib.
+FOUNDATION_EXPORT const unsigned char PsyDataLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PsyDataLib/PublicHeader.h>
+
+
---
+++ PsyDataLib/PsyDataLib.swift
@@ -0,0 +1,21 @@
+//
+//  PsyDataLib.swift
+//  PsyDataLib
+//
+//  Created by Oliver Larsen on 15/03/2019.
+//  Copyright © 2019 amsiq. All rights reserved.
+//
+
+import Foundation
+
+public class PsyDataLib {
+    
+   
+    let name = "PsyDataLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    
+}
